45f921b8eab58f7ecdcc71875091e4939233337e
sinan-894 <mohammedsinan1011@gmail.com>
2024-08-07 00:39:06 +0530

    To test the playRound()

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 
+let humanScore=0;
+let computerScore=0;
+
+
 function getComputerChoice(){
     let randomNum=Math.floor(Math.random()*3);
     if (randomNum==0){
@@ -13,6 +17,67 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let humanChoice=parseInt(prompt("choose one from below\n1-Rock\n2.Paper\n3.Scissor"))
-    console.log(humanChoice)
+    let humanChoice=parseInt(prompt("choose one from below\n1-Rock\n2-Paper\n3-Scissor"))
+    if (humanChoice==1){
+        return 'Rock';
+    }
+    else if (humanChoice==2){
+        return "Paper";
+    }
+    else if(humanChoice=3){
+        return "Scissor";
+    }
+    else{
+        console.log("enter a valid choice");
+        getHumanChoice();
+    }
+}
+
+function playRound(humanChoice,computerChoice){
+    humanChoice = humanChoice.toUpperCase();
+    computerChoice = computerChoice.toUpperCase();
+    switch(humanChoice){
+        case "ROCK":
+            if (computerChoice=='PAPER'){
+                console.log("You lose! Paper beats Rock");
+                computerScore+=1;
+            }
+            else if(computerChoice=='SCISSOR'){
+                console.log("You win! Rock beats Scissor");
+                humanScore+=1;
+            }
+            else{
+                console.log("DRAW!");
+            }
+            break;
+        case "PAPER":
+            if (computerChoice=='SCISSOR'){
+                console.log("You lose! Scissor beats Paper");
+                computerScore+=1;
+            }
+            else if(computerChoice=='ROCK'){
+                console.log("You win! Paper beats Rock");
+                humanScore+=1;
+            }
+            else{
+                console.log("DRAW!");
+            }
+            break;
+        case "SCISSOR":
+            if (computerChoice=='ROCK'){
+                console.log("You lose! Rock beats Scissor");
+                computerScore+=1;
+            }
+            else if(computerChoice=='PAPER'){
+                console.log("You win! Scissor beats Paper");
+                humanScore+=1;
+            }
+            else{
+                console.log("DRAW!");
+            }
+            break;
+
+
+    }
+
 }
