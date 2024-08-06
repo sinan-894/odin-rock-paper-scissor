50bf7b2e7d828e377c198ca88d103f8d531c608e
sinan-894 <mohammedsinan1011@gmail.com>
2024-08-07 00:14:56 +0530

    To test the getMachineChoice function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+
+function getComputerChoice(){
+    let randomNum=Math.floor(Math.random()*3);
+    if (randomNum==0){
+        return 'Rock';
+    }
+    else if (randomNum==1){
+        return "Paper";
+    }
+    else{
+        return "Scissor";
+    }
+}
